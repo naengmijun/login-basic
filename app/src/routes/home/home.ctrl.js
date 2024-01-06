f6eda4c7e5e1a198e77bb64e6bb6440f76e9d14f
이준호 <junho@ijunhoui-MacBookAir.local>
2024-01-06 12:26:19 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const users = {
+    id: ["woorimIT", "나개발", "김팀장"],
+    psword: ["1234", "1234", "123456"],
+};
+
 const output = {
     hello: (req, res) => {
         res.render("home/index");
@@ -12,9 +17,24 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+            psword = req.body.psword;
+        
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
     },
-}
+};
 
 module.exports = {
     output,
