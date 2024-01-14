a9ea5dfe50b80cb8dcd765e85f345c08f8125704
이준호 <junho@ijunhoui-MacBookAir.local>
2024-01-14 12:12:29 +0900

    로그 관리 모듈 winston 등록 및 설정

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,8 +1,10 @@
 "use strict";
 
 const app = require("../app");
+const logger = require("../src/config/logger");
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server is running on port 3000");
+    logger.info(`${PORT}번 포트에서 서버가 가동되었습니다.`);
 });
